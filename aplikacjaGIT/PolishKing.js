37e706ff398b2d85b602013db130eee52a7538e9
maciek.fol <maciej.folgmann@icloud.com>
2023-10-30 09:54:53 +0100

    first commit

---
+++ aplikacjaGIT/PolishKing.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const PolishKing = ()=>{
+
+    return(
+        <View>
+            <Text>Polski krol idzie</Text>
+        </View>
+    )
+}
+export default PolishKing;
